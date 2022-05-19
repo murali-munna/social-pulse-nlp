4836256ca6d55bd81d5c37aa043109d5c3a3df0b
Murali Mohana Krishna Dandu <murali.munna94@gmail.com>
2022-05-19 14:49:27 -0700

    streamlit demo app

---
+++ streamlit_app/demo.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+st.title("Welcome to Streamlit!")
+
+st.write("Line Chart in Streamlit")
+# 10 * 2 dimensional data
+chart_data = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=[f"Col{i+1}" for i in range(2)]
+)
+
+st.line_chart(chart_data)
