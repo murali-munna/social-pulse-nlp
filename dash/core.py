85960e2164ecd8ce10abae63c0454f847801cb43
TP <173043733@qq.com>
2022-05-28 21:39:35 -0700

    feat: better organized page selection.

--- dash/core.py
+++ dash/core.py
@@ -5,9 +5,6 @@ import pydeck as pdk
 
 from urllib.error import URLError
 
-# https://raw.githubusercontent.com/streamlit/example-data/master/hello/v1/bike_rental_stats.json
-
-# https://towardsdatascience.com/creating-multipage-applications-using-streamlit-efficiently-b58a58134030
 
 def display_template_geodata():
     try:
@@ -72,9 +69,10 @@ def display_template_geodata():
             Connection error: %s
         """ % e.reason)
 
+
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 5],
-  'second column': [12, 20, 30, 40]
+    'first column': [1, 2, 3, 5],
+    'second column': [12, 20, 30, 40]
 })
 
 
@@ -85,28 +83,38 @@ def from_data_file(filename):
         "example-data/master/hello/v1/%s" % filename)
     return pd.read_json(url)
 
+
 def display():
+    pageHandlers = {
+        "page1": lambda: st.write(df),
+        "page2": lambda: st.write(df),
+        "Geo Analysis": display_template_geodata,
+        "page4": lambda: st.write(df),
+        "page5": lambda: st.write(df),
+    }
     with st.sidebar:
-        choose = option_menu("App Gallery", ["df", "geo data", "Project Planning", "Course", "Contact"],
-                            icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
-                            menu_icon="app-indicator", default_index=0,
-                            styles={
+        choose = option_menu("App Gallery", list(pageHandlers.keys()),
+                             icons=['house', 'camera fill', 'kanban',
+                                    'book', 'person lines fill'],
+                             menu_icon="app-indicator", default_index=0,
+                             styles={
             "container": {"padding": "5!important", "background-color": "#fafafa"},
-            "icon": {"color": "orange", "font-size": "25px"}, 
-            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "icon": {"color": "orange", "font-size": "25px"},
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
             "nav-link-selected": {"background-color": "#02ab21"},
         }
         )
 
-    if choose == "df":
-        df
-    if choose == "geo data":
-        display_template_geodata()
+    (pageHandlers[choose])()
 
 
 def display_selectbox_style():
-    # Create a page dropdown 
-    page = st.sidebar.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"]) 
+    """
+    select box style page selection. NOT USING
+    """
+    # Create a page dropdown
+    page = st.sidebar.selectbox(
+        "Choose your page", ["Page 1", "Page 2", "Page 3"])
     if page == "Page 1":
         # Display details of page 1
         df
@@ -116,6 +124,7 @@ def display_selectbox_style():
     elif page == "Page 3":
         # Display details of page 3
         display_template_geodata()
+
 
 if __name__ == '__main__':
     display()
